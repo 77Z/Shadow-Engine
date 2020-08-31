f3f784bde4a943c9aadacf285e58ea212a8dc16e
Vince Richter <r2d2fan20@outlook.com>
2020-08-31 14:07:39 -0500

    upgrades!

--- scripts/webview/terminal.js
+++ scripts/webview/terminal.js
@@ -8,9 +8,8 @@ const TermLetterCellSize = {
 var term = new Terminal({
     theme: {
         background: "#160024"
-    }/* ,
-    fontFamily: "Terminal",
-    letterSpacing: -5 */
+    },
+    fontFamily: "Terminal"
 });
 term.open(document.getElementById("terminal"));
 term.write("Shadow Terminal\r\n\r\n");
