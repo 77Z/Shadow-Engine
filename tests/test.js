9d65fe784c65f0c9922231ac2bdfa4576c4c8466
Vince Richter <r2d2fan20@outlook.com>
2020-09-06 20:09:39 -0500

    Linux testing! YAY

---
+++ tests/test.js
@@ -0,0 +1,16 @@
+const child_process = require("child_process");
+const { stderr } = require("process");
+
+if (process.platform == "linux") {
+    console.log("running tests for linux");
+    process.exit(0);
+} else if (process.platform == "win32") {
+    console.log("running tests for win32");
+    child_process.exec("ShadowTests.exe", (error, stdout, stderr) => {
+        if (error) console.error(error);
+        console.log(stdout);
+        console.log(stderr);
+    });
+} else {
+    console.log("platform not supported :(");
+}
