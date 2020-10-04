018052fa1337096c121edf30647b2e83224c8cc5
Vince Richter <r2d2fan20@outlook.com>
2020-10-04 01:38:48 -0500

    Small quality of life changes and more read desc
    Future plans:
    
    I plan on using the localstorage feature instead of storing things like
    the currently open project in a file. I won't be moving it to
    localstorage unless it creates bugs in the future.

--- PostMaster/generatePostmaster.js
+++ PostMaster/generatePostmaster.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 
 module.exports = {
-    createPostmasterExecutable = function(settings, projectName, destination) {
+    createPostmasterExecutable: function(settings, projectName, destination) {
         if (typeof settings !== "object") throw new TypeError("settings must be an object");
         if (typeof projectName !== "string") throw new TypeError("projectName must be a string");
         if (typeof destination !== "string") throw new TypeError("destination must be a string");
